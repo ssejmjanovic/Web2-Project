fe3fbc7b3218b3f8319c3099bb8b9d595ec19636
ssejmjanovic <ssejmjanovic@gmail.com>
2026-08-13 01:52:17 +0200

    feat: add travel plans endpoints

---
+++ backend/TravelPlanner/TravelService/Controllers/ApiControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelService.Controllers
+{
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        protected int CurrentUserId => int.Parse(User.FindFirst("sub").Value);
+    }
+}
---
+++ backend/TravelPlanner/TravelService/Controllers/TravelPlansController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TravelService.DTOs;
+using TravelService.Services;
+
+namespace TravelService.Controllers
+{
+    [ApiController]
+    [Route("api/travel-plans")]
+    [Authorize]
+    public class TravelPlansController : ApiControllerBase
+    {
+        private readonly ITravelPlanService _travelPlanService;
+
+        public TravelPlansController(ITravelPlanService travelPlanService)
+        {
+            _travelPlanService = travelPlanService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TravelPlanSummaryDto>>> GetAll()
+            => Ok(await _travelPlanService.GetAllForUserAsync(CurrentUserId));
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TravelPlanDto>> GetById(int id)
+            => Ok(await _travelPlanService.GetByIdAsync(id, CurrentUserId));
+
+        [HttpPost]
+        public async Task<ActionResult<TravelPlanDto>> Create([FromBody] TravelPlanInputDto dto)
+        {
+            var created = await _travelPlanService.CreateAsync(CurrentUserId, dto);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TravelPlanDto>> Update(
+            int id, [FromBody] TravelPlanInputDto dto)
+            => Ok(await _travelPlanService.UpdateAsync(id, CurrentUserId, dto));
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _travelPlanService.DeleteAsync(id, CurrentUserId);
+            return NoContent();
+        }
+    }
+}
