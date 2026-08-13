22d7d44ac89c662e2b8a796bac951d69db62e7de
ssejmjanovic <ssejmjanovic@gmail.com>
2026-08-13 02:29:53 +0200

    feat: configure travel service dependency injection, jwt and swagger

--- backend/TravelPlanner/TravelService/Startup.cs
+++ backend/TravelPlanner/TravelService/Startup.cs
@@ -1,14 +1,18 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using TravelService.Data;
+using TravelService.Middleware;
+using TravelService.Services;
 
 namespace TravelService
 {
