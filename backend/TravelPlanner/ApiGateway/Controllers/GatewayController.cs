53f2d5af60c88c7063bb2f93ae981740a4393a1c
ssejmjanovic <ssejmjanovic@gmail.com>
2026-08-14 18:45:06 +0200

    feat: add request forwarding to gateway

--- backend/TravelPlanner/ApiGateway/Controllers/GatewayController.cs
+++ backend/TravelPlanner/ApiGateway/Controllers/GatewayController.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiGateway.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApiGateway.Controllers
 {
-    public class GatewayController
+    [ApiController]
+    public class GatewayController : ControllerBase
     {
+        private readonly IProxyService _proxyService;
+        private readonly IServiceRouteResolver _routeResolver;
+
+        public GatewayController(IProxyService proxyService, IServiceRouteResolver routeResolver)
+        {
+            _proxyService = proxyService;
+            _routeResolver = routeResolver;
+        }
+
+        [Route("api/{**path}")]
+        [AcceptVerbs("GET", "POST", "PUT", "PATCH", "DELETE")]
+        public async Task<IActionResult> Forward()
+        {
+            if (!_routeResolver.TryResolve(Request.Path.Value, out var targetBaseUrl))
+                return NotFound(new { message = "Unknown API route" });
+
+            using (var response = await _proxyService.ForwardAsync(Request, targetBaseUrl))
+            {
+                var body = await response.Content.ReadAsStringAsync();
+
+                return new ContentResult
+                {
+                    StatusCode = (int)response.StatusCode,
+                    Content = body,
+                    ContentType = response.Content.Headers.ContentType?.ToString() ?? "appication/json"
+                };
+            }
+        }
     }
 }
---
+++ backend/TravelPlanner/ApiGateway/Services/ProxyService.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiGateway.Services
+{
+
+    public interface IProxyService
+    {
+        Task<HttpResponseMessage> ForwardAsync(HttpRequest request, string targetBaseUrl);
+    }
+
+    public class ProxyService : IProxyService
+    {
+        private static readonly string[] ForwardedHeaders = { "Authorization", "X-Share-Token", "Accept" };
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProxyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<HttpResponseMessage> ForwardAsync(HttpRequest request, string targetBaseUrl)
+        {
+            var targetUrl = $"{targetBaseUrl.TrimEnd('/')}{request.Path}{request.QueryString}";
+
+            var forwarded = new HttpRequestMessage(new HttpMethod(request.Method), targetUrl);
+
+            if (request.ContentLength > 0)
+            {
+                using (var reader = new StreamReader(request.Body))
+                {
+                    var body = await reader.ReadToEndAsync();
+                    var mediaType = request.ContentType?.Split(';')[0] ?? "application/json";
+
+                    forwarded.Content = new StringContent(body, Encoding.UTF8, mediaType);
+                }
+            }
+            foreach(var headerName in ForwardedHeaders)
+            {
+                if (request.Headers.TryGetValue(headerName, out var values))
+                    forwarded.Headers.TryAddWithoutValidation(headerName, values.ToArray());
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            return await client.SendAsync(forwarded, HttpCompletionOption.ResponseHeadersRead);
+        }
+    }
+}
