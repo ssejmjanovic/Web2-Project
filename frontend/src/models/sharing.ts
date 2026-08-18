4096aeed4bfbe2d518bf38f5fe78eed5a5c9fa77
ssejmjanovic <ssejmjanovic@gmail.com>
2026-08-18 14:06:21 +0200

    fix: fixed a typo in sharing model

--- frontend/src/models/sharing.ts
+++ frontend/src/models/sharing.ts
@@ -11,7 +11,7 @@ export interface Share {
 
 export interface CreateShareRequest {
     travelPlanId: number;
-    acessLevel: ShareAccessLevel;
+    accessLevel: ShareAccessLevel;
     expiresInHours?: number;
 }
 
