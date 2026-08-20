be3375af1f16a2abfafbb24b04919aafbc3045a9
ssejmjanovic <ssejmjanovic@gmail.com>
2026-08-20 15:35:23 +0200

    chore: remove duplicate using directives

--- backend/TravelPlanner/UserService/Services/UserManagementService.cs
+++ backend/TravelPlanner/UserService/Services/UserManagementService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using UserService.Data;
 using UserService.DTOs;
